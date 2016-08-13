2fbb24b6f142f954b433d547b41d97f4ced58951
Seylerius <seylerius@publicinquisition.com>
2016-08-13 11:38:46 -0700

    Initial commit

---
+++ test/com/seriouslyseylerius/org_collect_test.clj
@@ -0,0 +1,7 @@
+(ns com.seriouslyseylerius.org-collect-test
+  (:require [clojure.test :refer :all]
+            [com.seriouslyseylerius.org-collect :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
