0b22b21e98335b0180e4374ad130917070d38fb6
Seylerius <seylerius@publicinquisition.com>
2016-08-14 01:45:12 -0700

    Added command line options

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [me.raynes/fs "1.4.6"]]
+                 [me.raynes/fs "1.4.6"]
+                 [org.clojure/tools.cli "0.3.5"]]
   :main ^:skip-aot com.seriouslyseylerius.org-collect
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/com/seriouslyseylerius/org_collect.clj
+++ src/com/seriouslyseylerius/org_collect.clj
@@ -1,7 +1,29 @@
 (ns com.seriouslyseylerius.org-collect
   (:require [me.raynes.fs :as fs]
-            [clojure.set :as sets])
+            [clojure.set :as sets]
+            [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
+
+(declare cli)
+
+(def cli-options
+  [["-t" "–target TARGET" "Target directory"
+    :default "~/orgzly"
+    :parse-fn #(fs/expand-home %)
+    :validate [#(fs/exists? %) "Target directory must exist"]]
+   ["-d" "–default DEFAULT" "Default directory"
+    :default "~/notes"
+    :parse-fn #(fs/expand-home %)
+    :validate [#(fs/exists? %) "Default directory must exist"]]
+   ["-v" nil "Verbosity level"
+    :id :verbosity
+    :default 0
+    :assoc-fn (fn [m k _] (update-in m [k] inc))]
+   ["-q" "nil" "Verbosity level"
+    :id :verbosity
+    :default 0
+    :assoc-fn (fn [m k _] (update-in m [k] dec))]
+   ["-h" "–help"]])
 
 (defn select-folders
   "Expand a potentially globbed list of folders"
@@ -29,34 +51,42 @@
 (defn sync-files
   "Sync the provided list of files with the destination folder"
   [source-files dest-folder orphan-folder]
-  (println "Got " (count source-files) " source files…")
+  (if (> 0 (-> cli :options :vebosity))
+    (println "Got " (count source-files) " source files…"))
   (let [dest-files (remove fs/hidden? (fs/list-dir
                                        (fs/expand-home dest-folder)))]
-    (println "Got " (count dest-files)" destination files…")
+    (if (> 0 (-> cli :options :vebosity))
+      (println "Got " (count dest-files)" destination files…"))
     (let [orphans (sets/difference (set (map fs/base-name dest-files))
                                    (set (map fs/base-name source-files)))]
-      (println "Got " (count orphans) " orphans…")
+      (if (> 0 (-> cli :options :vebosity))
+        (println "Got " (count orphans) " orphans…"))
       (doseq [source-file source-files]
-        (println "Checking " source-file "…")
+        (if (> 2 (-> cli :options :vebosity))
+          (println "Checking " source-file "…"))
         (let [dest-file (str dest-folder "/" (fs/base-name source-file))]
           (let [dest-time (fs/mod-time dest-file)
                 source-time (fs/mod-time source-file)]
             (cond (> source-time dest-time)
                   (do
-                    (println (str "Copying " source-file
-                                  " to " dest-file "…"))
+                    (if (> 1 (-> cli :options :vebosity))
+                      (println (str "Copying " source-file
+                                    " to " dest-file "…")))
                     (fs/copy source-file dest-file))
                   (< source-time dest-time)
                   (do
-                    (println (str "Copying " dest-file
-                                  " to " source-file))
+                    (if (> 1 (-> cli :options :vebosity))
+                      (println (str "Copying " dest-file
+                                    " to " source-file)))
                     (fs/copy dest-file source-file))))))
       (doseq [orphan-file (map (partial str dest-folder "/") orphans)]
         (let [orphan-dest (str orphan-folder "/" (fs/base-name orphan-file))]
-          (println "Copying " orphan-file " to " orphan-dest)
+          (if (> 1 (-> cli :options :vebosity))
+            (println "Copying " orphan-file " to " orphan-dest))
           (fs/copy orphan-file orphan-dest))))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Synchronize org files from specified directories with the target folder."
   [& args]
-  (println "Hello, World!"))
+  (def cli (parse-opts args cli-options))
+  (sync-files (get-org-files (concat [(-> cli :options :default)] (cli :arguments))) (-> cli :options :target) (-> cli :options :default)))
