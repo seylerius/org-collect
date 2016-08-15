830465b800e5191efc843831637d5f1e3431ba8a
Seylerius <seylerius@publicinquisition.com>
2016-08-14 19:17:03 -0700

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.seriouslyseylerius/org-collect "0.1.0-SNAPSHOT"
+(defproject com.seriouslyseylerius/org-collect "1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
